c75b22f9b0233de9cdec56e06d8b6df15ef73848
Kinvalan <sebastianmoller01@icloud.com>
2023-05-28 21:38:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Lag en konsoll applikasjon hvor man kan generere opp flere forskjellige virtuelle kjæledyr. 
+
+Bruk constructor i klassen, og man skal ikke ha lov til å endre data som navn, age eller noen andre characteristics utenfra klassen. 
+
+Lag funksjonalitet som gjør at man kan gi dyret mat, 
+Bruk en overload til å gi en annen feedback til console dersom det er favorittmaten til dyret 
+- f eks “Hurra dette er det beste jeg vet tusen takk for maten!” istedenfor f.eks “Takk for maten”. 
+*/
+
+
+namespace Ukesoppgave_uke3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FeedPet();
+        }
+
+
+        static void FeedPet()
+        {
+            var dog = new VirtualPet(name:"Bingo", age:7, personality:"Tullete");
+
+            Console.WriteLine($"Hva skal gi gi til {dog.Name}?");
+
+            var userInput = Console.ReadLine();
+
+            string feedback = dog.FeedPet(userInput);
+
+            Console.WriteLine(feedback);
+        }
+    }
+}
---
+++ VirtualPet.cs
@@ -0,0 +1,36 @@
+﻿namespace Ukesoppgave_uke3;
+
+public class VirtualPet
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public string Personality { get; private set; }
+
+
+    public VirtualPet(string name, int age, string personality)
+    {
+        Name = name;
+        Age = age;
+        Personality = personality;
+    }
+
+    public string FeedPet(string food)
+    {
+        return FeedPet(food, false);
+    }
+
+    public string FeedPet(string food, bool isFavorite)  // Overload
+    {
+        if (food.ToLower() == "kylling")
+        {
+            return "Hurra, dette er det beste jeg vet! Tusen takk for maten!";
+        }
+        else
+        {
+            return "Takk for maten.";
+        }
+    }
+
+
+
+}
